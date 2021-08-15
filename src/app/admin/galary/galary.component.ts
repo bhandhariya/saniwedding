0ce3468d42a19e39cac0abea298ff977bd667e2a
raja saini <sainiraja59@gmail.com>
2021-08-15 17:31:53 +0530

    galary done

--- src/app/admin/galary/galary.component.ts
+++ src/app/admin/galary/galary.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FileUploadService } from 'src/app/service/file-upload.service';
+import Swal from 'sweetalert2';
+import { FileUpload } from "../../component/navbar/fileuplad";
 @Component({
   selector: 'app-galary',
   templateUrl: './galary.component.html',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalaryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private uploadService: FileUploadService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  selectedFiles?: FileList;
+  currentFileUpload?: FileUpload;
+  percentage = 0;
+  selectFile(event: any): void {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload(): void {
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      this.selectedFiles = undefined;
+
+      if (file) {
+        this.currentFileUpload = new FileUpload(file);
+        this.uploadService.pushFileToStorageforGalary(this.currentFileUpload).subscribe(
+          percentage => {
+            this.percentage = Math.round(percentage ? percentage : 0);
+            if(percentage==100){
+              Swal.fire('done','Your image Uploaded','success')
+            }
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }
+    }
+
+  }
+
+  reset(){
+    this.percentage=0;
+    this.selectedFiles=null;
+  }
 }
